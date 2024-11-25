97a3778a4e044196c51c3dd5120f3931a701a9c7
junsu <junsu1211@gmail.com>
2024-11-25 17:39:29 +0900

    Force update

--- car_platform_app/lib/src/shared/global.dart
+++ car_platform_app/lib/src/shared/global.dart
@@ -1,4 +1,4 @@
 class Global {
   static String accessToken = '';
-  static const String baseUrl = 'https://test.gyudol.click:3000';
+  static const String baseUrl = 'https://dev.gyudol.click:3000';
 }
